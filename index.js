a460279062ec2929376de26ed3e8b27eb9c06ce3
predator27 <anand.naman997@gmail.com>
2025-02-26 19:04:39 +0530

    Step 2: Add skeleton routes for user

--- index.js
+++ index.js
@@ -4,29 +4,39 @@ const app = express()
 // route skeleton for user login, signup, purchase a course, see course
 
 
-app.post("/signup", function (req,res){
-    // the user sends us the data to signup 
-    // we store the user info at our database 
-
-    const email = req.body.email
-    const name = req.body.name
-})
-
-app.post("/signin", function (req,res){
-    // the user sends us the data to signin 
-
-    const email = req.body.email
-    const name = req.body.name
+app.post("/user/signup", function (req,res){
+    res.json({
+        message: "signed up endpoint"
+    })
 })
 
 
-app.post("/course/purchase", function (req,res){
+app.post("/user/signin", function (req,res){
+    res.json({
+        message: "sign in endpoint"
+    })
+})
+
+
+// endpoint for user to hit when he wants a new course 
+app.post("/course/purchase", function(req,res){
+    // this endpoint is to buy new courses by the user
+
+    // we are not adding payment thingy to buy new course in this project yet.
+    // user hits this endpoint and that way he purchases it 
+})
+
+
+// endpoint for user to get all their purchased courses
+app.get("/user/purchases", function (req,res){
 
 })
 
-app.get("/course/see", fucntion(req,res){
+
+// endpoint for user to see all the courses 
+app.get("/courses", function (req,res){
 
 })
 
-
+app.listen(3000)
 
