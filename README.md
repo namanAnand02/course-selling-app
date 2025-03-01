1655ce305a02b5c961932b2a8463370b9eb8bc2f
predator27 <anand.naman997@gmail.com>
2025-03-01 22:51:32 +0530

    hash password, i/p validate using zod

--- README.md
+++ README.md
@@ -9,6 +9,25 @@
     - NOTE:  we did one improvements in step 5: we wrapped database connection and listening on port inside one func, we await database connection first, and on success, we listen on port 3000.
     - Another improvement is about using dotenv, something i'll learn and do later.
 
+- step 6: write the business logic for admin 
+
+    - used hashing of password using bcrypt lib, zod for i/p validation, adminModel.create({..}) to save data into db
+    - /signup endpoint 
+        - user send email, password, first and last name. We access it from req.
+        - we hash the password --> use await bcrypt.hash(plainpassword, saltRounds). And we store this hashed password into the database and not that plain password.
+        - store the info into database --> await adminModel.create({pass diff schema based input data})
+        - at last, we respond back to admin user that he signed up. 
+
+    - /signin endpoint 
+        - user again sends email and password and we access them using req. 
+        - we first check if any admin with this email exists or not?! we dont check for password into the database as this is plain text password and we have saved hashed one into db during signup.
+        - if admin exists with that email, we then compare password --> use bcrypt.compare(plainPass, HashedPass), we access saved hashedPass from admin using admin.password.
+        - only when password matches, we create a JWT token for this admin user using jsonwebtoken --> jwt.sign(payload,JWT_SECRET)
+        - and we respond back to the user with this token
+
+        - NOTE: to learn cookie based auth and session based auth.
+
+
 - add middlewares for user and admin auth 
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,8 @@ const { adminModel, userModel } = require("../db")
 const { userRouter } = require("./user")
 const jwt = require("jsonwebtoken") // import jwt 
 const JWT_SECRET = "s3cret" // generate a jwt secret 
+const bcrypt = require("bcrypt")
+const { z } = require("zod")
 
 
 ///adminRouter.use(express.json()) // not needed here - only sufficient in index.js file 
@@ -13,19 +15,53 @@ const JWT_SECRET = "s3cret" // generate a jwt secret
 // admin signup
 adminRouter.post("/signup", async (req,res)=> {
     // adminSchema has email, password, fristName and lastName
+    // use hashing for password
+
+    // validate the input using zod 
+    const requireBody = z.object({
+        email: z.string().min(7).max(20).email(),
+        password: z.string().min(7).max(20),
+        firstName: z.string().min(7).max(20),
+        lastName: z.string().min(7).max(20)
+    })
+
+    // input validation using zod step2 : safeParse
+    const parsedDataWithSuccess = requireBody.safeParse(req.body)
+
+    // if parsedDataWithSuccess was a success, proceed with the usual proceedings, else, respoind back the user the problem 
+
+    if (!parsedDataWithSuccess.success){
+        // safeParse gave error, so we respond back to the user about the problem 
+        // safeParse gives us .success and .error functions 
+
+        res.json({
+            message: "Incorrect i/p data",
+            error: parsedDataWithSuccess.error
+        })
+
+    }
+    // else parsedData using safeParse was a success, business as usual
+
+
     const email = req.body.email;
     const password = req.body.password;
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
 
+    // hash the password before storing them into the database 
+    const hashedPassword = await bcrypt.hash(password, 5)
+    console.log(hashedPassword);
+    
+
     //store them in database 
     // we have already defined adminModel whose sole work is to put data into admins collection inside database following the admin schema 
 
     await adminModel.create({
-        email,
-        password,
-        firstName,
-        lastName
+        email: email,
+        //password: password, // dont save plain password into the database
+        password: hashedPassword, // save salted hashed password into the database 
+        firstName: firstName,
+        lastName: lastName
     })
 
     // respond back to the user that they have signed up.
@@ -55,7 +91,7 @@ this is what gets saved inside mongoDB coursera-app-database --> admins collecti
 */
 
 
-// admin login 
+// admin signin 
 adminRouter.post('/signin', async (req,res)=>{
 
     // 1. we ask user to sign in using email and password
@@ -65,15 +101,35 @@ adminRouter.post('/signin', async (req,res)=>{
     const email = req.body.email
     const password = req.body.password
 
+    // we first check database for email existence or not
+    // we won't send plain password to be matched in database, as we had saved hashed password into the database 
+
     const admin = await adminModel.findOne({
         email: email,
-        password: password
+        //// password: password
     })  
 
-    console.log(admin);
-    
+    // if a user admin exists with this email, its all info gets saved into admin variable
 
-    if (admin){
+    console.log(admin);
+
+    // if database has none admin with this email id, we respond back- wrong creadentials
+    if (!admin){
+        res.status(403).json({
+            message: "admin with this email doesn't exist."
+        })
+    }
+
+    // if yes, an admin with that email exists, as a next step, we match the password
+    
+    //  we match the hashed password 
+    // we will use bcrypt.compare(plain password, saved hashed password) - it does all the back door work- and tell us the final result 
+
+    // comparing the password of the user admin
+    const passwordMatched = await bcrypt.compare(password, admin.password)
+
+
+    if (passwordMatched){
         // generate token using jsonwebtoken - jwt 
         // install jsonwebtoken 
         // import jwt from jsonwebtoken
